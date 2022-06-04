f930a40865ad80a4bd75857648d6acbaff740f7a
perezAnder <pma2019514@est.univalle.edu>
2022-06-03 22:35:38 -0400

    subiendo inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import Navbar from './componentes/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <h1>Hello word</h1>
+    </div>
+  );
+}
+
+export default App;
